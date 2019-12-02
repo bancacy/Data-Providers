8edafeaa707de404447cd2ba8b2a3fe277d5add3
AndyPuppyZ <44590470+AndyPuppyZ@users.noreply.github.com>
2019-12-02 18:26:01 +0200

    Flat verison

--- contracts/MedianOracle.sol
+++ contracts/MedianOracle.sol
@@ -1,9 +1,317 @@
-pragma solidity 0.5.11;
+pragma solidity ^0.5.1;
 
-import "../openzeppelin-solidity/contracts/math/SafeMath.sol";
-import "../openzeppelin-solidity/contracts/ownership/Ownable.sol";
+/**
+ * @dev Wrappers over Solidity's arithmetic operations with added overflow
+ * checks.
+ *
+ * Arithmetic operations in Solidity wrap on overflow. This can easily result
+ * in bugs, because programmers usually assume that an overflow raises an
+ * error, which is the standard behavior in high level programming languages.
+ * `SafeMath` restores this intuition by reverting the transaction when an
+ * operation overflows.
+ *
+ * Using this library instead of the unchecked operations eliminates an entire
+ * class of bugs, so it's recommended to use it always.
+ */
+library SafeMath {
+    /**
+     * @dev Returns the addition of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `+` operator.
+     *
+     * Requirements:
+     * - Addition cannot overflow.
+     */
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, "SafeMath: addition overflow");
 
-import "./lib/Select.sol";
+        return c;
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, reverting on
+     * overflow (when the result is negative).
+     *
+     * Counterpart to Solidity's `-` operator.
+     *
+     * Requirements:
+     * - Subtraction cannot overflow.
+     */
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        return sub(a, b, "SafeMath: subtraction overflow");
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, reverting with custom message on
+     * overflow (when the result is negative).
+     *
+     * Counterpart to Solidity's `-` operator.
+     *
+     * Requirements:
+     * - Subtraction cannot overflow.
+     *
+     * _Available since v2.4.0._
+     */
+    function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b <= a, errorMessage);
+        uint256 c = a - b;
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the multiplication of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `*` operator.
+     *
+     * Requirements:
+     * - Multiplication cannot overflow.
+     */
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+        // benefit is lost if 'b' is also tested.
+        // See: https://github.com/OpenZeppelin/openzeppelin-contracts/pull/522
+        if (a == 0) {
+            return 0;
+        }
+
+        uint256 c = a * b;
+        require(c / a == b, "SafeMath: multiplication overflow");
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the integer division of two unsigned integers. Reverts on
+     * division by zero. The result is rounded towards zero.
+     *
+     * Counterpart to Solidity's `/` operator. Note: this function uses a
+     * `revert` opcode (which leaves remaining gas untouched) while Solidity
+     * uses an invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     * - The divisor cannot be zero.
+     */
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        return div(a, b, "SafeMath: division by zero");
+    }
+
+    /**
+     * @dev Returns the integer division of two unsigned integers. Reverts with custom message on
+     * division by zero. The result is rounded towards zero.
+     *
+     * Counterpart to Solidity's `/` operator. Note: this function uses a
+     * `revert` opcode (which leaves remaining gas untouched) while Solidity
+     * uses an invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     * - The divisor cannot be zero.
+     *
+     * _Available since v2.4.0._
+     */
+    function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        // Solidity only automatically asserts when dividing by 0
+        require(b > 0, errorMessage);
+        uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+     * Reverts when dividing by zero.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     * - The divisor cannot be zero.
+     */
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        return mod(a, b, "SafeMath: modulo by zero");
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+     * Reverts with custom message when dividing by zero.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     * - The divisor cannot be zero.
+     *
+     * _Available since v2.4.0._
+     */
+    function mod(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b != 0, errorMessage);
+        return a % b;
+    }
+}
+
+
+/*
+ * @dev Provides information about the current execution context, including the
+ * sender of the transaction and its data. While these are generally available
+ * via msg.sender and msg.data, they should not be accessed in such a direct
+ * manner, since when dealing with GSN meta-transactions the account sending and
+ * paying for execution may not be the actual sender (as far as an application
+ * is concerned).
+ *
+ * This contract is only required for intermediate, library-like contracts.
+ */
+contract Context {
+    // Empty internal constructor, to prevent people from mistakenly deploying
+    // an instance of this contract, which should be used via inheritance.
+    constructor () internal { }
+    // solhint-disable-previous-line no-empty-blocks
+
+    function _msgSender() internal view returns (address ) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view returns (bytes memory) {
+        this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
+        return msg.data;
+    }
+}
+
+
+
+
+/**
+ * @dev Contract module which provides a basic access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyOwner`, which can be applied to your functions to restrict their use to
+ * the owner.
+ */
+contract Ownable is Context {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor () internal {
+        _owner = _msgSender();
+        emit OwnershipTransferred(address(0), _owner);
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(isOwner(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    /**
+     * @dev Returns true if the caller is the current owner.
+     */
+    function isOwner() public view returns (bool) {
+        return _msgSender() == _owner;
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public onlyOwner {
+        emit OwnershipTransferred(_owner, address(0));
+        _owner = address(0);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public onlyOwner {
+        _transferOwnership(newOwner);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     */
+    function _transferOwnership(address newOwner) internal {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        emit OwnershipTransferred(_owner, newOwner);
+        _owner = newOwner;
+    }
+}
+
+
+
+
+
+
+/**
+ * @title Select
+ * @dev Median Selection Library
+ */
+library Select {
+    using SafeMath for uint256;
+
+    /**
+     * @dev Sorts the input array up to the denoted size, and returns the median.
+     * @param array Input array to compute its median.
+     * @param size Number of elements in array to compute the median for.
+     * @return Median of array.
+     */
+    function computeMedian(uint256[] memory array, uint256 size)
+        internal
+        pure
+        returns (uint256)
+    {
+        require(size > 0 && array.length >= size);
+        for (uint256 i = 1; i < size; i++) {
+            for (uint256 j = i; j > 0 && array[j-1]  > array[j]; j--) {
+                uint256 tmp = array[j];
+                array[j] = array[j-1];
+                array[j-1] = tmp;
+            }
+        }
+        if (size % 2 == 1) {
+            return array[size / 2];
+        } else {
+            return array[size / 2].add(array[size / 2 - 1]) / 2;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 interface IOracle {
@@ -23,7 +331,7 @@ contract MedianOracle is Ownable, IOracle {
     struct Report {
         uint256 timestamp;
         uint256 payload;
-        bool main;
+        
     }
 
     // Addresses of providers authorized to push reports.
@@ -156,16 +464,24 @@ contract MedianOracle is Ownable, IOracle {
     * @return AggregatedValue: Median of providers reported values.
     *         valid: Boolean indicating an aggregated value was computed successfully.
     */
+    
+            address MainAddress;
+            uint256 public Where = 0;
+        uint256 public index = 0;
+        uint256 public mainCount =0;
+         uint256 public regularNodes;
+        uint256  public size = 0;
+        address public nodeAddress;
+        uint256 public nodeIndex;
+        uint256[]    validReports;
+
     function getData()
         external
         returns (uint256, bool)
     {
-        address MainAddress;
-        uint256 index = 0;
-        uint256 mainCount =0;
-        uint256 reportsCount = providers.length;
-        uint256[] storage validReports = new uint256[](reportsCount);
-        uint256 size = 0;
+        
+        uint256   reportsCount = providers.length;
+        
         uint256 minValidTimestamp =  now.sub(reportExpirationTimeSec);
         uint256 maxValidTimestamp =  now.sub(reportDelaySec);
 
@@ -178,14 +494,17 @@ contract MedianOracle is Ownable, IOracle {
             uint256 reportTimestampRecent = reports[index_recent].timestamp;
             if (reportTimestampRecent > maxValidTimestamp) {
                 // Recent report is too recent.
+                Where++;
                 uint256 reportTimestampPast = providerReports[providerAddress][index_past].timestamp;
                 if (reportTimestampPast < minValidTimestamp) {
                     // Past report is too old.
+                    Where=2;
                     emit ReportTimestampOutOfRange(providerAddress);
                 } else if (reportTimestampPast > maxValidTimestamp) {
                     // Past report is too recent.
+                    Where=3;
                     emit ReportTimestampOutOfRange(providerAddress);
-                } else {
+                } else { Where = 4;
                     // Using past report.
                     validReports[size++] = providerReports[providerAddress][index_past].payload;
                     for (uint256 j = 0; j < mainProviders.length; j++) {
@@ -193,19 +512,21 @@ contract MedianOracle is Ownable, IOracle {
                         MainAddress  = mainProviders[j];
                         index = index_past;
                         mainCount++;
-                        isMain = true;
                         }
+                        if(mainProviders[j] != providerAddress){
+                         nodeAddress  = mainProviders[j];
+                         nodeIndex = index_past;
+                        }
+                        
                     }
-                    if(!isMain){
-                    validReports[size++] = providerReports[MainAddress][index].payload;
-                    }
+                    
                 }
-            } else {
+            } else { Where=5;
                 // Recent report is not too recent.
-                if (reportTimestampRecent < minValidTimestamp) {
+                if (reportTimestampRecent < minValidTimestamp) { Where=6;
                     // Recent report is too old.
                     emit ReportTimestampOutOfRange(providerAddress);
-                } else {
+                } else {Where=7;
                     // Using recent report.
                     validReports[size++] = providerReports[providerAddress][index_recent].payload;
                     for (uint256 j = 0; j < mainProviders.length; j++) {
@@ -215,8 +536,8 @@ contract MedianOracle is Ownable, IOracle {
                         mainCount++;
                         }
                         if(mainProviders[j] != providerAddress){
-                        address nodeAddress  = mainProviders[j];
-                        uint256 nodeIndex = index_past;
+                         nodeAddress  = mainProviders[j];
+                         nodeIndex = index_past;
                         }
                         
                     }
@@ -228,7 +549,7 @@ contract MedianOracle is Ownable, IOracle {
             return (0, false);
         }
 
-        uint256 regularNodes = validReports.length - mainCount;
+         regularNodes = validReports.length - mainCount;
         if(regularNodes == 0 || mainCount == 0 )
         {
           return (0, false);
@@ -265,6 +586,13 @@ contract MedianOracle is Ownable, IOracle {
         providerReports[provider][0].timestamp = 1;
         emit ProviderAdded(provider);
     }
+    function addMainProvider(address provider)
+        external
+        onlyOwner
+    {
+        mainProviders.push(provider);
+        emit ProviderAdded(provider);
+    }
 
     /**
      * @notice Revokes provider authorization.
@@ -298,3 +626,4 @@ contract MedianOracle is Ownable, IOracle {
         return providers.length;
     }
 }
+
